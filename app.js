19350e6ef3b0a4fccc722921850b836eec77d58c
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2024-01-17 22:05:46 -0500

    Quick questions complete

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("hi");
+const quickQuestionOne = new Set([1, 1, 2, 2, 3, 4]);
+
+console.log(quickQuestionOne); // Set {1,2,3,4}
+
+// Quick Question 2
+const quickQuestionTwo = [...new Set("referee")].join("");
+
+console.log(quickQuestionTwo); // ref
+
+// Quick Question 3
+
+let m = new Map();
+m.set([1, 2, 3], true); // [1,2,3] => true
+m.set([1, 2, 3], false); // [1,2,3] => true, [1,2,3] => false
+
+m.size; // 2
+// An array and obj are instances so if you say [] = [] it will return false bcuz they are diff instances so the results above will create two keys and two values. It does not reassign true to false
+
+console.log(m);
